8684d2644e3675b558576b02e915d2bd21136c69
Zhou-Xun <48509321+Zhou-Xun@users.noreply.github.com>
2021-04-27 11:28:15 +0800

    Delete twitter_secret.py

--- data/analysis/twitter_secret.py
+++
@@ -1,8 +0,0 @@
-#fill this in with your credentials and rename to secrets.py
-#make sure to gitignore this file! you will lose points if you share this in your github!
-
-TWITTER_API_KEY="yJTyFYlphrbRE3qgpCKMKQmBi"
-TWITTER_API_SECRET="81k5IRuXpxELTQGwuI9ScByIlH19BMzQqiVPTR3OvBRHSjmSdU"
-TWITTER_ACCESS_TOKEN="1374569768821264385-YjPqVvA5yEPJEFImRxvwrG4wiGxAgU"
-TWITTER_ACCESS_TOKEN_SECRET="vPz7iWNyptZGlq6NHl4D71unIsuz4yFBRDPZIdQFNQP97"
-
